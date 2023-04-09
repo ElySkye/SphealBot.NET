ae76ed627e5e0dce3117d78c4d4554dbf3c63adc
ElySkye <findingnemo65@gmail.com>
2023-04-09 22:46:09 +0800

    Revert "minor changes"
    This reverts commit af0ee47cd100b4e1d1119bcf9cb33d4c91374111.

--- SysBot.Pokemon/LA/BotTrade/PokeTradeBotLA.cs
+++ SysBot.Pokemon/LA/BotTrade/PokeTradeBotLA.cs
@@ -742,7 +742,7 @@ namespace SysBot.Pokemon
                 if (cd != 0 && TimeSpan.FromMinutes(cd) > delta)
                 {
                     poke.Notifier.SendNotification(this, poke, "You have ignored the trade cooldown set by the bot owner. The owner has been notified.");
-                    var msg = $"Found NPC on {user.TrainerName}{useridmsg} ignoring the {cd} minute trade cooldown. Last encountered {delta.TotalMinutes:F1} minutes ago.";
+                    var msg = $"Found {user.TrainerName}{useridmsg} ignoring the {cd} minute trade cooldown. Last encountered {delta.TotalMinutes:F1} minutes ago.";
                     if (AbuseSettings.EchoNintendoOnlineIDCooldown)
                         msg += $"\nOT: {TrainerName}";
                     msg += $"\nID: {TrainerNID}";
